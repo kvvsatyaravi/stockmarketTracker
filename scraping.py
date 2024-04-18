65c027655ad157a19faa03c5c6dadc5c84237f10
kvvsatyaravi <kvvsatyaravi318@gmail.com>
2024-04-18 12:35:16 +0530

    live bse&nse script

--- scraping.py
+++ scraping.py
@@ -17,34 +17,34 @@ driver.quit()
 
 ### code for extracting mutaul funds table data
 
-import requests
-from bs4 import BeautifulSoup
-import json
+# import requests
+# from bs4 import BeautifulSoup
+# import json
 
-class Api:
-    """
-    A class used to store constants
-    """
-    def __init__(self, title_info=None, link_info=None, date_info=None, news_info=None):
-        """
-        Initializes the constants
-        """
-        self.html_parser = "html.parser"
-        self.url = [
-            "https://www.moneycontrol.com/mutual-funds/quant-small-cap-fund-direct-plan/portfolio-overview/MES056"
-        ]
-        self.AllData = ["https://www.niftyindices.com/market-data/equity-stock-watch?Iname=NIFTY%20TOTAL%20MKT"]
+# class Api:
+#     """
+#     A class used to store constants
+#     """
+#     def __init__(self, title_info=None, link_info=None, date_info=None, news_info=None):
+#         """
+#         Initializes the constants
+#         """
+#         self.html_parser = "html.parser"
+#         self.url = [
+#             "https://www.moneycontrol.com/mutual-funds/quant-small-cap-fund-direct-plan/portfolio-overview/MES056"
+#         ]
+#         self.AllData = ["https://www.niftyindices.com/market-data/equity-stock-watch?Iname=NIFTY%20TOTAL%20MKT"]
 
 
-# dummy = {}
+# # dummy = {}
 
-# soup = BeautifulSoup(requests.get(Api().url[0], timeout=60).text, Api().html_parser)
-soup_allData = BeautifulSoup(requests.get(Api().AllData[0], timeout=60).text, Api().html_parser)
+# # soup = BeautifulSoup(requests.get(Api().url[0], timeout=60).text, Api().html_parser)
+# soup_allData = BeautifulSoup(requests.get(Api().AllData[0], timeout=60).text, Api().html_parser)
 
-# soup_body = soup.select("table#equityCompleteHoldingTable > tbody > tr > td")
-# soup_header = soup.select("table#equityCompleteHoldingTable > thead > tr > th")
+# # soup_body = soup.select("table#equityCompleteHoldingTable > tbody > tr > td")
+# # soup_header = soup.select("table#equityCompleteHoldingTable > thead > tr > th")
 
-print(soup_allData)
+# print(soup_allData)
 
 
 # setData = []
--- server/stockmarketapi/views.py
+++ server/stockmarketapi/views.py
@@ -4,12 +4,105 @@ from rest_framework.response import Response
 import requests
 from bs4 import BeautifulSoup
 import json
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
 import datetime
 import time
+# import FetchAllData
 
+
+class Api:
+    """
+    A class used to store constants
+    """
+    def __init__(self):
+        """
+        Initializes the constants
+        """
+        self.xmlparser = "lxml"
+        self.htmlparser = "html.parser"
+        self.url = [
+            "https://www.moneycontrol.com/mutual-funds/quant-small-cap-fund-direct-plan/portfolio-overview/MES056"
+        ]
+        self.bseAllData = "https://www.moneycontrol.com/markets/indian-indices/changeTableData?deviceType=web&exName=B&indicesID=67&selTab=o&subTabOT=o&subTabOPL=cl&selPage=marketTerminal&classic=true"
+        self.nseAllData = "https://www.moneycontrol.com/markets/indian-indices/changeTableData?deviceType=web&exName=N&indicesID=136&selTab=o&subTabOT=o&subTabOPL=cl&selPage=marketTerminal&classic=true"
+
+    def scrapingWebsite(self,response):
+
+        soup_allData = BeautifulSoup(response.text, self.htmlparser)
+
+        for eachElement in soup_allData.find_all('th'):
+            if eachElement.get_text().strip() == 'Analysis'  or eachElement.get_text().strip() == 'Technical Rating':
+                eachElement.decompose()
+
+        
+        for eachElement in soup_allData.find_all('td'):
+            if eachElement.button  or (eachElement.a and eachElement.a.attrs['href'] == "javascript:;"):
+                eachElement.decompose()
+
+        soup_header = soup_allData.select("table > thead > tr > th")
+        soup_body = soup_allData.select('tbody > tr > td')
+
+        setData = []
+        count=0
+
+        print(soup_body)
+
+        for x in soup_body:
+            if count <= len(soup_body)-1:
+                dummy = {}
+                for y in soup_header:
+                    dummy[y.get_text().strip()] = soup_body[count].get_text().replace('/n','').strip()
+                    count = count + 1
+                
+                setData.append(dummy)
+
+            # print(setData)
+
+        return setData
+
+
+    def fetchBseAllData(self):
+        payload = {}
+        headers = {}
+        response = requests.request("GET", self.bseAllData, headers=headers, data=payload)
+        Data = self.scrapingWebsite(response)
+        return Data
+        # with open('data/bseAllData.json', 'w') as my_file:
+        #     json.dump(setData, my_file,indent = 4)
+
+    
+    def fetchNseAllData(self):
+        payload = {}
+        headers = {}
+        response = requests.request("GET", self.nseAllData, headers=headers, data=payload)
+        Data = self.scrapingWebsite(response)
+        return Data
+        
+        
+fetchAllData = Api()
+
+
+@api_view(['GET'])
+def allStocksData(request):
+    startTime = time.time()
+    
+    Data = fetchAllData.fetchNseAllData()
+    
+    endTime = time.time()
+
+    # get the execution time
+    elapsed_time = int(endTime - startTime)
+    elapsed_time = str(datetime.timedelta(seconds=elapsed_time))
+    print('Execution time:', elapsed_time)
+    
+    responseData = {
+        "result":"success",
+        "data":Data,
+        "stocksCount":len(Data),
+        "lastUpdated":str(datetime.datetime.now()),
+        "executedTime":elapsed_time
+    }
+
+    return Response(responseData)
 
 @api_view(['GET'])
 def allStocksData(request):
