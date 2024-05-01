77d28df766d3c9c2240f6537de9265f1a74f7acd
kvvsatyaravi <kvvsatyaravi318@gmail.com>
2024-05-01 12:52:50 +0530

    new changes

--- server/stockmarketapi/Apis.py
+++ server/stockmarketapi/Apis.py
@@ -15,8 +15,10 @@ class Api:
         self.url = [
             "https://www.moneycontrol.com/mutual-funds/quant-small-cap-fund-direct-plan/portfolio-overview/MES056"
         ]
-        self.bseAllData = "https://www.moneycontrol.com/markets/indian-indices/changeTableData?deviceType=web&exName=B&indicesID=67&selTab=o&subTabOT=o&subTabOPL=cl&selPage=marketTerminal&classic=true"
-        self.nseAllData = "https://www.moneycontrol.com/markets/indian-indices/changeTableData?deviceType=web&exName=N&indicesID=136&selTab=o&subTabOT=o&subTabOPL=cl&selPage=marketTerminal&classic=true"
+        self.bseAllData = ["https://www.moneycontrol.com/markets/indian-indices/changeTableData?deviceType=web&exName=B&indicesID=67&selTab=o&subTabOT=o&subTabOPL=cl&selPage=marketTerminal&classic=true",
+                           "https://www.moneycontrol.com/markets/indian-indices/changeTableData?deviceType=web&exName=B&indicesID=104&selTab=o&subTabOT=o&subTabOPL=cl&selPage=marketTerminal&classic=true",
+                           "https://www.moneycontrol.com/markets/indian-indices/changeTableData?deviceType=web&exName=B&indicesID=106&selTab=o&subTabOT=o&subTabOPL=cl&selPage=marketTerminal&classic=true"]
+        self.nseAllData = ["https://www.moneycontrol.com/markets/indian-indices/changeTableData?deviceType=web&exName=N&indicesID=136&selTab=o&subTabOT=o&subTabOPL=cl&selPage=marketTerminal&classic=true"]
 
     def scrapingWebsite(self,response):
 
@@ -32,31 +34,31 @@ class Api:
                 eachElement.decompose()
 
         soup_header = soup_allData.select("table > thead > tr > th")
-        soup_body = soup_allData.select('tbody > tr > td')
+        soup_body = soup_allData.select('tbody > tr')
 
-        setData = []
+        soup_data = soup_allData.select('tbody > tr > td')
+
+        setData = {}
         count=0
 
-        print(soup_body)
+        # print(soup_body)
 
         for x in soup_body:
-            if count <= len(soup_body)-1:
-                dummy = {}
-                for y in soup_header:
-                    if soup_body[count].a:
-                        link = soup_body[count].a['href']
-                        name = link.replace("https://www.moneycontrol.com/india/stockpricequote/","")
-                        name = name.split("/")
-                        print(name[1])
-                        dummy[y.get_text().strip()] = name[1]
-                        count = count + 1
-                    else:
-                        dummy[y.get_text().strip()] = soup_body[count].get_text().replace('/n','').strip()
-                        count = count + 1
-                
-                setData.append(dummy)
+            dummy = {}
+            name = ""
+            for y in soup_header:
+                if soup_data[count].a:
+                    link = soup_data[count].a['href']
+                    name = link.replace("https://www.moneycontrol.com/india/stockpricequote/","")
+                    name = name.split("/")
+                    # print(name[1])
+                    dummy[y.get_text().strip()] = name[1]
+                    count = count + 1
+                else:
+                    dummy[y.get_text().strip()] = soup_data[count].get_text().replace('/n','').strip()
+                    count = count + 1
 
-            # print(setData)
+            setData[name[1]] = dummy
 
         return setData
 
@@ -64,8 +66,12 @@ class Api:
     def fetchBseAllData(self):
         payload = {}
         headers = {}
-        response = requests.request("GET", self.bseAllData, headers=headers, data=payload)
-        setData = self.scrapingWebsite(response)
+        setData = {}
+        for i in self.bseAllData:
+            response = requests.request("GET", i, headers=headers, data=payload)
+            data = self.scrapingWebsite(response)
+            print(data)
+            setData.update(data)
         
         return setData
 
@@ -73,57 +79,51 @@ class Api:
     def fetchNseAllData(self):
         payload = {}
         headers = {}
-        print("testing nse data")
-        response = requests.request("GET", self.nseAllData, headers=headers, data=payload)
-        setData = self.scrapingWebsite(response)
+        setData = {}
+        for i in self.nseAllData:
+            response = requests.request("GET", i, headers=headers, data=payload)
+            data = self.scrapingWebsite(response)
+            setData.update(data)
         
         return setData
         
     def MutualFundsTracker(self,url):
-        print(url)
         mutualFund = "quantsmallcap"
         fundUrl = "https://www.moneycontrol.com/mutual-funds/tata-digital-india-fund-direct-plan/portfolio-overview/MTA1147"
 
         html_parser = "html.parser"
-        # url = [
-        #     "https://www.moneycontrol.com/mutual-funds/tata-digital-india-fund-direct-plan/portfolio-overview/MTA1147"
-        # ]
-
-
-        # dummy = {}
 
         soup = BeautifulSoup(requests.get(url, timeout=60).text, html_parser)
-        # soup_allData = BeautifulSoup(requests.get(Api().AllData[0], timeout=60).text, Api().html_parser)
+        soup_data = soup.select("table#equityCompleteHoldingTable tbody :not(tr[style='display: none;']) td")
 
-        soup_body = soup.select("table#equityCompleteHoldingTable > tbody > tr > td")
+        soup_body = soup.select("table#equityCompleteHoldingTable tbody tr:not(tr[style='display: none;'])")
         soup_header = soup.select("table#equityCompleteHoldingTable > thead > tr > th")
 
         setData = []
 
-        titles = soup.select("span.port_right > a")
-        print(titles)
+        titles = soup.select("table#equityCompleteHoldingTable tbody :not(tr[style='display: none;']) td span.port_right a")
 
-        print(soup_body)
+        # print(soup_body)
+
         count=0
         titlesCount = 0
 
         for x in soup_body:
-            if count <= len(soup_body)-1:
-                dummy = {}
-                for y in soup_header:
-                    if y.get_text().strip() == 'Stock Invested in':
-                        link = titles[titlesCount]['href']
-                        name = link.replace("https://www.moneycontrol.com/india/stockpricequote/","")
-                        name = name.split("/")
-                        # print(name[1])
-                        dummy[y.get_text().strip()] = name[1]
-                        titlesCount = titlesCount + 1
-                        count = count + 1
-                    else:
-                        dummy[y.get_text().strip()] = soup_body[count].get_text().replace('/n','').strip()
-                        count = count + 1
+            dummy = {}
+            for y in soup_header:
+                if y.get_text().strip() == 'Stock Invested in':
+                    link = titles[titlesCount]['href']
+                    name = link.replace("https://www.moneycontrol.com/india/stockpricequote/","")
+                    name = name.split("/")
+                    # print(name)
+                    
+                    dummy[y.get_text().strip()] = name[1]
+                    titlesCount = titlesCount +1
+                    count = count + 1
+                else:
+                    dummy[y.get_text().strip()] = soup_data[count].get_text().replace('/n','').strip()
+                    count = count + 1
 
-                setData.append(dummy)
-        print(setData)
+            setData.append(dummy)
         
         return setData
