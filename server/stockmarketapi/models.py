4742b17e0b5847f07a19c9ab5430d3327939d828
kvvsatyaravi <kvvsatyaravi318@gmail.com>
2025-05-06 22:54:18 +0530

    model created db

--- server/stockmarketapi/models.py
+++ server/stockmarketapi/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    UserID = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=10)
+    password = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.username
+
+class StockInfo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_column='UserID')
+    EditDate = models.DateField()
+    TargetPrice = models.FloatField()
+    Priority = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.EditDate}"
